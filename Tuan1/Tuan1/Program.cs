ad697148d0457f5a4fdfd69759a8e3cfc858ec93
Nguyễn Văn Nguyên <100029793+nguyen1o2@users.noreply.github.com>
2023-03-19 12:44:54 +0700

    Add files via upload

--- Tuan1/Tuan1/Program.cs
+++ Tuan1/Tuan1/Program.cs
@@ -4,15 +4,49 @@ namespace Tuan1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Nhap(int []a,int n)
         {
-            Console.Write("Nhap chieu dai: ");
-            int a = int.Parse(Console.ReadLine());
-            Console.Write("Nhap chieu rong: ");
-            int b = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("a[" + i + "]= ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+        }
 
-            Console.WriteLine("Chu vi HCN = "+((a + b) * 2));
-            Console.WriteLine("Dien tich HCN = "+(a*b));
+        public static void Xuat(int []a,int n)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + "\t");
+            }
+        }
+        public static void Sapxeptang(int []a,int n)
+        {
+            for(int i = 0; i < n - 1; i++)
+            {
+                for(int j = i + 1; j < n; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {   int []a;
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());         
+            a = new int[n];
+            Nhap(a, n);
+            Console.WriteLine("Mang vua nhap la");
+            Xuat(a, n);
+            Sapxeptang(a, n);
+            Console.WriteLine("\nMang sau khi sap xep la");
+            Xuat(a, n);
+            Console.ReadLine();
         }
     }
 }
