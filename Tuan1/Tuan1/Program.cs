f2d0f10d5ce83bd1a8762fb4ab2846ad4f5d1b83
Nguyễn Văn Nguyên <100029793+nguyen1o2@users.noreply.github.com>
2023-03-19 11:27:49 +0700

    Add files via upload

---
+++ Tuan1/Tuan1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tuan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap chieu dai: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap chieu rong: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Chu vi HCN = "+((a + b) * 2));
+            Console.WriteLine("Dien tich HCN = "+(a*b));
+        }
+    }
+}
