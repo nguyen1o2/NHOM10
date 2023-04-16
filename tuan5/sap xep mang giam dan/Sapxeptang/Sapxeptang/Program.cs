027a2f1682b55c45c37305ee982550450a0fcd92
Nguyễn Văn Nguyên <100029793+nguyen1o2@users.noreply.github.com>
2023-04-16 17:27:41 +0700

    Add files via upload

---
+++ tuan5/sap xep mang giam dan/Sapxeptang/Sapxeptang/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Sapxeptang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[10];
+
+            int n, i, j, tmp; //tmp la bien tam (bien trung gian)
+
+
+            Console.Write("\nSap xep mang theo thu tu tang dan trong C#:\n");
+            Console.Write("--------------------------------------------\n");
+
+            Console.Write("Nhap kich co mang: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nhap {0} phan tu vao trong mang:\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Phan tu arr1[{0}]: ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < n; i++)
+            {
+                for (j = i + 1; j < n; j++)
+                {
+                    if (arr1[i] > arr1[j])
+                    {
+                        //cach trao doi gia tri
+                        tmp = arr1[i];
+                        arr1[i] = arr1[j];
+                        arr1[j] = tmp;
+                    }
+                }
+            }
+
+            Console.Write("\nIn cac phan tu mang theo thu tu tang dan:\n");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("{0}  ", arr1[i]);
+            }
+            Console.Write("\n\n");
+
+            Console.ReadKey();
+        }
+    }
+}
