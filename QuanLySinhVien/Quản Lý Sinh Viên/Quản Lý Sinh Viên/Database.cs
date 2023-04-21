5c137df7857e74cdc0495d4399b9c5490357f45a
Nguyễn Văn Nguyên <100029793+nguyen1o2@users.noreply.github.com>
2023-04-21 22:10:58 +0700

    Add files via upload

---
+++ QuanLySinhVien/Quản Lý Sinh Viên/Quản Lý Sinh Viên/Database.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quản_Lý_Sinh_Viên
+{
+    class Database
+    {
+        private string connectionString = @"Data Source=DESKTOP-IMD6DBM;Initial Catalog=QLSV;Integrated Security=True";
+        private SqlConnection conn;
+        private string sql;
+        private DataTable dt;
+        private SqlCommand cmd;
+        public Database()
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("connected failed " + ex.Message);
+            }
+        }
+
+        public DataTable SelectData()
+        {
+            try
+            {
+                conn.Open();
+                sql = "select * from dbo.tblSinhVien";
+                cmd = new SqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Loi load du lieu: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
