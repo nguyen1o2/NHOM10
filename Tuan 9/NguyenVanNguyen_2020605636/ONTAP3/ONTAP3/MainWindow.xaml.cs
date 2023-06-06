b7b0f4dd009053382164e6b0ebe447f12f70fd8e
Nguyễn Văn Nguyên <100029793+nguyen1o2@users.noreply.github.com>
2023-06-06 13:39:28 +0700

    Add files via upload

---
+++ Tuan 9/NguyenVanNguyen_2020605636/ONTAP3/ONTAP3/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ONTAP3.Models;
+using System.Reflection;
+namespace ONTAP3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        QLBHContext db = new QLBHContext();
+
+        private void HienThiDuLieu()
+        {
+            var query = from sp in db.SanPhams
+                        orderby sp.DonGia
+                        select new
+                        {
+                            sp.MaSp,
+                            sp.TenSp,
+                            sp.MaLoai,
+                            sp.SoLuong,
+                            sp.DonGia,
+                            ThanhTien = sp.DonGia * sp.SoLuong
+                        };
+            dgvSanPham.ItemsSource = query.ToList();
+        }
+
+        private void HienThiCB()
+        {
+            var query = from lsp in db.LoaiSanPhams
+                        select lsp;
+            cboloaiSP.ItemsSource = query.ToList();
+            cboloaiSP.DisplayMemberPath = "TenLoai";
+            cboloaiSP.SelectedValuePath = "MaLoai";
+            cboloaiSP.SelectedIndex = 0;
+        }
+
+        private void Window_Loaded(object sender,RoutedEventArgs e)
+        {
+            HienThiDuLieu();
+            HienThiCB();
+        }
+
+        private void btnthem_Click(object sender, RoutedEventArgs e)
+        {
+            var query = (from sp in db.SanPhams
+                         where sp.MaSp == txtmaSP.Text
+                         select sp).SingleOrDefault();
+            if (query != null)
+            {
+                MessageBox.Show("Ma san pham da ton tai", "Thong bao");
+                HienThiDuLieu();
+            }
+            else
+            {
+                SanPham spmoi = new SanPham();
+                spmoi.MaSp = txtmaSP.Text;
+                spmoi.TenSp = txttenSP.Text;
+                spmoi.SoLuong = int.Parse(txtsoLuong.Text);
+                spmoi.DonGia = float.Parse(txtdonGia.Text);
+                LoaiSanPham s = (LoaiSanPham)cboloaiSP.SelectedItem;
+                spmoi.MaLoai = s.MaLoai;
+                db.SanPhams.Add(spmoi);
+                db.SaveChanges();
+                MessageBox.Show("Them thanh cong", "Thong bao");
+                HienThiDuLieu();
+            }
+        }
+
+        private void btnsua_Click(object sender, RoutedEventArgs e)
+        {
+            /*var spsua = (from sp in db.SanPhams
+                         where sp.MaSp == txtmaSP.Text
+                         select sp).SingleOrDefault();*/
+            var spsua = db.SanPhams.SingleOrDefault(t => t.MaSp.Equals(txtmaSP.Text));
+            if (spsua != null)
+            {
+                spsua.TenSp = txttenSP.Text;
+                spsua.SoLuong = int.Parse(txtsoLuong.Text);
+                spsua.DonGia = float.Parse(txtdonGia.Text);
+                LoaiSanPham s = (LoaiSanPham)cboloaiSP.SelectedItem;
+                spsua.MaLoai = s.MaLoai;
+                db.SaveChanges();
+                MessageBox.Show("Sua thanh cong");
+                HienThiDuLieu();
+            }
+            else
+            {
+                MessageBox.Show("Ma san pham khong ton tai", "Thong bao");
+            }          
+        }
+
+        private void btnxoa_Click(object sender, RoutedEventArgs e)
+        {
+            var spxoa = db.SanPhams.SingleOrDefault(t => t.MaSp.Equals(txtmaSP.Text));
+            if(spxoa == null)
+            {
+                MessageBox.Show("Ma san pham khong ton tai", "thong bao");
+            }
+            else
+            {
+                db.SanPhams.Remove(spxoa);
+                db.SaveChanges();
+                MessageBox.Show("Xoa thanh cong", "Thong bao");
+                HienThiDuLieu();
+            }
+        }
+
+        private void btnthongke_Click(object sender, RoutedEventArgs e)
+        {
+            Window mywindow = new Window();
+            mywindow.Show();
+        }
+
+        private void dgvSanPham_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(dgvSanPham.SelectedItem != null)
+            {
+                try
+                {
+                    Type t = dgvSanPham.SelectedItem.GetType();
+                    PropertyInfo[] p = t.GetProperties();
+                    txtmaSP.Text = p[0].GetValue(dgvSanPham.SelectedValue).ToString();
+                    txttenSP.Text = p[1].GetValue(dgvSanPham.SelectedValue).ToString();
+                    cboloaiSP.SelectedValue = p[2].GetValue(dgvSanPham.SelectedValue).ToString();
+                    txtsoLuong.Text = p[3].GetValue(dgvSanPham.SelectedValue).ToString();
+                    txtdonGia.Text = p[4].GetValue(dgvSanPham.SelectedValue).ToString();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Loi chon hang " + ex.Message, "Thong bao");
+                }
+            }
+        }
+
+        private void btntim_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from sp in db.SanPhams
+                        where sp.MaSp == txtmaSP.Text
+                        select new
+                        {
+                            sp.MaSp,
+                            sp.TenSp,
+                            sp.MaLoai,
+                            sp.SoLuong,
+                            sp.DonGia,
+                            ThanhTien = sp.DonGia * sp.SoLuong
+                        };
+            dgvSanPham.ItemsSource = query.ToList();
+        }
+    }
+}
---
+++ Tuan 9/NguyenVanNguyen_2020605636/ONTAP3/ONTAP3/Models/QLBHContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ONTAP3.Models
+{
+    public partial class QLBHContext : DbContext
+    {
+        public QLBHContext()
+        {
+        }
+
+        public QLBHContext(DbContextOptions<QLBHContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<LoaiSanPham> LoaiSanPhams { get; set; }
+        public virtual DbSet<SanPham> SanPhams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-IMD6DBM;Initial Catalog=QLBH;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<LoaiSanPham>(entity =>
+            {
+                entity.HasKey(e => e.MaLoai)
+                    .HasName("PK__LoaiSanP__730A57598DE48BA0");
+
+                entity.ToTable("LoaiSanPham");
+
+                entity.Property(e => e.MaLoai)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.TenLoai).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<SanPham>(entity =>
+            {
+                entity.HasKey(e => e.MaSp)
+                    .HasName("PK__SanPham__2725081CC26C83FD");
+
+                entity.ToTable("SanPham");
+
+                entity.Property(e => e.MaSp)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("MaSP")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.MaLoai)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.TenSp)
+                    .HasMaxLength(50)
+                    .HasColumnName("TenSP");
+
+                entity.HasOne(d => d.MaLoaiNavigation)
+                    .WithMany(p => p.SanPhams)
+                    .HasForeignKey(d => d.MaLoai)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK__SanPham__MaLoai__267ABA7A");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
